6e31aa332a94665c59906aa19f46229be94fac2c
aalexren <chernicaar@gmail.com>
2023-03-14 02:28:18 +0300

    some pipes

--- 2/solution/solution.c
+++ 2/solution/solution.c
@@ -44,19 +44,50 @@ add_name_to_argv(char *name, char **argv, int argc)
 int
 exec_cmds(struct cmd **comms, int count)
 {
+    /* Make pipes */
+    int **pipes = NULL;
+    int len = 0; int pipe_idx = 0;
     for (int i = 0; i < count; ++i)
     {
+        if (cmd_get_special(comms[i]) == 0)
+        {
+            len++;
+            pipes = realloc(pipes, sizeof(int*) * len);
+            if (pipes == NULL)
+            {
+                fprintf(stderr, "Error during fork() call: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            pipes[len-1] = malloc(sizeof(int) * 2);
+            if (pipe(pipes[len-1]) == -1)
+            {
+                fprintf(stderr, "Error during fork() call: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    /* Save all pids to wait for them later */
+    pid_t *pids = malloc(sizeof(*pids) * len);
+
+    for (int i = 0; i < count; ++i)
+    {
+        if (strcmp(cmd_get_name(comms[i]), "|") == 0)
+        {
+            continue;
+        }
+
+        if (strcmp(cmd_get_name(comms[i]), "exit") == 0)
+        {
+            return 1;
+        }
         /* cd command handling */
-        if (strcmp(cmd_get_name(comms[i]), "cd") == 0)
+        else if (strcmp(cmd_get_name(comms[i]), "cd") == 0)
         {
             if (chdir(cmd_get_argv(comms[i])[0]) != 0)
             {
                 fprintf(stderr, "cd: no such file or directory: %s\n", cmd_get_argv(comms[i])[0]);
             }
-        }
-        else if (strcmp(cmd_get_name(comms[i]), "exit") == 0)
-        {
-            return 1;
         }
         else 
         {
@@ -71,6 +102,36 @@ exec_cmds(struct cmd **comms, int count)
             }
             else if (child_pid == 0)
             {
+                /* FILE DESCRIPTORS KEEPED GLOABLLY BY OS */
+                if (pipe_idx > 0)
+                {
+                    /* в предудщий писать не надо, закрываем */
+                    close(pipes[pipe_idx-1][1]);
+                    /**
+                     * перенаправляем читать stdin из предыдущего fd;
+                     * создаём новый дескриптор, который ассоциирован с newfd;
+                     * 
+                     * читаем из предыдущего;
+                     */
+                    dup2(pipes[pipe_idx-1][0], STDIN_FILENO);
+                    /* закрываем старый дескриптор предыдущего на чтение */
+                    close(pipes[pipe_idx-1][0]);
+                }
+                if (pipe_idx + 1 < len)
+                {
+                    /* из текущего нечего читать ещё, закрываем */
+                    close(pipes[pipe_idx][0]);
+                    /**
+                     * перенаправляем выводить stdout из текущего fd;
+                     * создаём новый дескриптор, который ассоциирован с newfd;
+                     * 
+                     * выводим в текущий;
+                     */
+                    dup2(pipes[pipe_idx][1], STDOUT_FILENO);
+                    /* закрыаем старый дескриптор текущего pipe на запись */
+                    close(pipes[pipe_idx][1]);
+                }
+
                 name = cmd_get_name(comms[i]);
                 args = cmd_get_argv(comms[i]);
                 argc = cmd_get_argc(comms[i]);
@@ -83,22 +144,42 @@ exec_cmds(struct cmd **comms, int count)
             }
             else 
             {
-                int status;
-                waitpid(child_pid, &status, 0);
+                pipe_idx++;
+                pids[pipe_idx-1] = child_pid;
                 // printf("Child process finished with status code %d\n", WEXITSTATUS(status));
-                
             }
             
-            if (name_args != NULL)
-            {
-                for (int j = 0; j < argc + 1; ++j)
-                {
-                    free(name_args[j]);
-                }
-                free(name_args);
-            }
+            // if (name_args != NULL)
+            // {
+            //     for (int j = 0; j < argc + 1; ++j)
+            //     {
+            //         free(name_args[j]);
+            //     }
+            //     free(name_args);
+            // }
         }
     }
+
+    /* Close all pipes */
+    for (int i = 0; i < len; ++i)
+    {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
+    for (int i = 0; i < len; ++i)
+    {
+        int status;
+        waitpid(pids[i], &status, 0);
+    }
+
+    /* Free allocated memory */
+    for (int i = 0; i < len; ++i)
+    {
+        free(pipes[i]);
+    }
+    free(pipes);
+    free(pids);
     
     return 0;
 }
@@ -152,24 +233,6 @@ int main(int argc, char **argv)
 
     heaph_init();
 
-    // struct char_stack *s = cs_init();
-    // struct char_stack *sr = NULL;
-    // // for (int i = 0; i < 10; ++i)
-    // // {
-    // //     cs_push(s,'a');cs_push(s,'b');cs_push(s,'c');
-    // //     sr = cs_reverse(s);
-
-    // //     char *ss = cs_splice(sr);
-    // //     free(ss);
-
-    // //     cs_free(sr);
-    // //     cs_free(s);
-    // //     free(sr);
-    // //     sr = NULL;
-    // // }
-    // // free(s);
-    // // printf("%p", sr);
-    // if (sr != NULL) free(sr);
     shell_loop();
 
     printf("Number of not freed allocations: %llu\n", heaph_get_alloc_count());
